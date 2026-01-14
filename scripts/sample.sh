4bedff8b5c9454b30202ef94e5162768def8c803
Sam Xu <samxucmu@gmail.com>
2026-01-13 16:20:29 -0800

    Add files via upload

---
+++ scripts/sample.sh
@@ -0,0 +1,3 @@
+python -m autobrep.inference.sample_ar \
+    --input_json configs/sample.json \
+    --output_dir samples --debug_dir samples
---
+++ scripts/train.sh
@@ -0,0 +1,3 @@
+
+python core/src/autobrep/train.py fit --config configs/autobrep.yaml \
+  --trainer.accelerator=gpu --trainer.devices=1 --trainer.strategy=ddp
