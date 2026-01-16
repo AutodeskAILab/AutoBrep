e7777be12f7c39e90d0c792c0825478bb7d060ae
Sam Xu <samxucmu@gmail.com>
2026-01-15 22:17:01 -0800

    Change strategy from ddp to fsdp

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,3 @@
 
 python core/src/autobrep/train.py fit --config configs/autobrep.yaml \
-  --trainer.accelerator=gpu --trainer.devices=1 --trainer.strategy=ddp
+  --trainer.accelerator=gpu --trainer.devices=1 --trainer.strategy=fsdp
